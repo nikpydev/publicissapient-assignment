a7dd58bd1f82b68278d6d47e91dd7e62f7619d50
Nikhil Choudhary <nc1336282@gmail.com>
2020-10-24 10:23:03 +0530

    Updates

---
+++ precache-manifest.bebaf7562c30382824d81e9f3cbaec50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05ae7c054bef1bdfeee794503e581c1",
+    "url": "/publicissapient-assignment/index.html"
+  },
+  {
+    "revision": "1137d095467639de7a85",
+    "url": "/publicissapient-assignment/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "d28a2247cf6d732a9554",
+    "url": "/publicissapient-assignment/static/css/main.e7b31dd1.chunk.css"
+  },
+  {
+    "revision": "1137d095467639de7a85",
+    "url": "/publicissapient-assignment/static/js/2.775fcf25.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/publicissapient-assignment/static/js/2.775fcf25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28a2247cf6d732a9554",
+    "url": "/publicissapient-assignment/static/js/main.91b8dcd0.chunk.js"
+  },
+  {
+    "revision": "4610c881dcea1bcd9b9e",
+    "url": "/publicissapient-assignment/static/js/runtime-main.563673f6.js"
+  }
+]);
